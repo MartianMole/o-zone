220cbce59e4443a9214e8d12a0110d71c2eecd8b
MartianMole <54107673+MartianMole@users.noreply.github.com>
2019-08-15 01:54:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,89 +1,147 @@
-
 'use strict';
 
-//checkbox
-
-const checkbox = document.querySelectorAll('.filter-check_checkbox'); 
-
-checkbox.forEach(function(elem) {
-        elem.addEventListener('change', function() {
-        if(this.checked) {
-            this.nextElementSibling.classList.add('checked');
-        } else {
-            this.nextElementSibling.classList.remove('checked');
-        }
-    });
-});
-
-//end checkbox
 
 
+function toggleCheckbox() {
+	const checkbox = document.querySelectorAll('.filter-check_checkbox');
 
-//bucket
-
-const btnCart = document.getElementById('cart');
-const modalCart = document.querySelector('.cart');
-const closeBtn = document.querySelector('.cart-close');
-
-btnCart.addEventListener('click', () => {
-    modalCart.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
-});
-
-closeBtn.addEventListener('click', () => {
-    modalCart.style.display = '';
-    document.body.style.overflow = '';
-});
-
-//end bucket
-
-
-
-//stuff in bucket
-
-const cards = document.querySelectorAll('.goods .card');
-const cartWrapper = document.querySelector('.cart-wrapper');
-const cartEmpty = document.getElementById('cart-empty');
-const countGoods = document.querySelector('.counter');
-
-/*
-
-
-
-    !!!!!!ВНИМАНИЕ!!!!!!
-
-    ЭТОТ КОММЕНТАРИЙ И ВСЕ ЧТО НИЖЕ - МОЯ САМОДЕЯТЕЛЬНОСТЬ
-
-    Это моя реализация общей суммы товаров в корзине
-    Надеюсь, не слишком криво
-
-
-
-*/
-
-
-//const cartTotal = document.querySelector('.cart-total span');
-
-cards.forEach((card) => {
-    const btn = card.querySelector('button');
-    btn.addEventListener('click', () => {
-        const cardClone = card.cloneNode(true);
-        cartWrapper.appendChild(cardClone);
-        cartEmpty.remove();
-        showData();
-    });
-
-});
-
-function showData() {
-    const cardsCart = cartWrapper.querySelectorAll('.card');
-    countGoods.textContent = cardsCart.length;
-    //let res = 0;
-    //cardsCart.forEach((card) => {
-        //const price = card.querySelector('.card-price');
-        //res += Number(price.textContent.slice(0, 5))
-    //});
-    //cartTotal.textContent = res;
+	checkbox.forEach(function (elem) {
+		elem.addEventListener('change', function () {
+			if (this.checked) {
+				this.nextElementSibling.classList.add('checked');
+			} else {
+				this.nextElementSibling.classList.remove('checked');
+			}
+		});
+	});
 }
 
-//end stuff in bucket
+
+
+function toggleCart() {
+	const btnCart = document.getElementById('cart');
+	const modalCart = document.querySelector('.cart');
+	const closeBtn = document.querySelector('.cart-close');
+
+	btnCart.addEventListener('click', () => {
+		modalCart.style.display = 'flex';
+		document.body.style.overflow = 'hidden';
+	});
+
+	closeBtn.addEventListener('click', () => {
+		modalCart.style.display = '';
+		document.body.style.overflow = '';
+	});
+}
+
+
+
+function addCart() {
+
+	const cards = document.querySelectorAll('.goods .card'),
+		cartWrapper = document.querySelector('.cart-wrapper'),
+		cartEmpty = document.getElementById('cart-empty'),
+		countGoods = document.querySelector('.counter');
+
+	cards.forEach((card) => {
+		const btn = card.querySelector('button');
+		btn.addEventListener('click', () => {
+			const cardClone = card.cloneNode(true);
+			cartWrapper.appendChild(cardClone);
+			showData();
+
+			const removeBtn = cardClone.querySelector('.btn');
+			removeBtn.textContent = "Удалить из корзины";
+			removeBtn.addEventListener("click", () => {
+				cardClone.remove();
+				showData();
+			});
+		});
+
+	});
+
+	function showData() {
+
+		const cardsCart = cartWrapper.querySelectorAll('.card'),
+			cardsPrice = cartWrapper.querySelectorAll('.card-price'),
+			cardTotal = document.querySelector('.cart-total span');
+		let sum = 0;
+
+		countGoods.textContent = cardsCart.length;
+
+		cardsPrice.forEach((cardPrice) => {
+			let price = parseFloat(cardPrice.textContent);
+			sum += price;
+		});
+
+		cardTotal.textContent = sum;
+
+		if (cardsCart.length !== 0) {
+			cartEmpty.remove();
+		} else {
+			cartWrapper.appendChild(cartEmpty);
+		}
+
+	}
+
+}
+
+
+
+function actionPage() {
+
+	const cards = document.querySelectorAll('.goods .card'),
+		discountCheckbox = document.getElementById('discount-checkbox'),
+		min = document.getElementById('min'),
+		max = document.getElementById('max'),
+		search = document.querySelector('.search-wrapper_input'),
+		searchBtn = document.querySelector('.search-btn');
+
+	discountCheckbox.addEventListener('click', () => {
+		cards.forEach((card) => {
+			if (discountCheckbox.checked) {
+				if (!card.querySelector('.card-sale')) {
+					card.parentNode.style.display = "none";
+				}
+			} else {
+				card.parentNode.style.display = '';
+			}
+		});
+	});
+	
+	min.addEventListener("change", filterPrice);
+	max.addEventListener("change", filterPrice);
+	
+	function filterPrice() {
+		cards.forEach((card) => {
+			const cardPrice = card.querySelector('.card-price');
+			const price = parseFloat(cardPrice.textContent);
+			if((min.value && price < min.value) || (max.value && price > max.value)) {
+				card.parentNode.style.display = "none";
+			} else {
+				card.parentNode.style.display = '';
+			}
+			
+		});
+	}
+
+	searchBtn.addEventListener('click', () => {
+		const searchText = new RegExp(search.value.trim(), 'i');
+		cards.forEach((card) => {
+			const title = card.querySelector('.card-title');
+			if(!searchText.test(title.textContent)) {
+				card.parentNode.style.display = 'none';
+			} else {
+				card.parentNode.style.display = '';
+			}
+		});
+	});
+
+}
+
+
+
+toggleCheckbox();
+toggleCart();
+addCart();
+actionPage();
